b25b8726554c2927710d9cd7a35289b68d7b429f
Niamh <85398097+niamh-d@users.noreply.github.com>
2025-11-13 21:21:05 +0200

    capturing entered new expense title

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -8,6 +8,14 @@ class NewExpense extends StatefulWidget {
 }
 
 class _NewExpenseState extends State<NewExpense> {
+  final _titleController = TextEditingController();
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -15,8 +23,17 @@ class _NewExpenseState extends State<NewExpense> {
       child: Column(
         children: [
           TextField(
+            controller: _titleController,
             maxLength: 50,
             decoration: const InputDecoration(label: Text('Title')),
+          ),
+          Row(
+            children: [
+              ElevatedButton(
+                onPressed: () => print(_titleController.text),
+                child: const Text('Save Expense'),
+              ),
+            ],
           ),
         ],
       ),

9da5dfdb2a63477ba830342ad95f19c52f39d22b
Niamh <85398097+niamh-d@users.noreply.github.com>
2025-11-16 18:21:09 +0200

    full screen new expense pop up

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -35,6 +35,7 @@ class _ExpensesState extends State<Expenses> {
 
   void _openAddExpenseOverlay() {
     showModalBottomSheet(
+      isScrollControlled: true,
       context: context,
       builder: (ctx) => NewExpense(onAddExpense: _addExpense),
     );

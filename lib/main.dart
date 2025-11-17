a960641b4f49ffc88a8003cf80c6d7547a3fba24
Niamh <85398097+niamh-d@users.noreply.github.com>
2025-11-17 11:35:18 +0200

    card, btn, text themes

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:expense_tracker/widgets/expenses.dart';
 var kColorScheme = ColorScheme.fromSeed(
   seedColor: const Color.fromARGB(255, 96, 59, 181),
 );
+
 void main() {
   runApp(
     MaterialApp(
@@ -13,6 +14,22 @@ void main() {
         appBarTheme: const AppBarTheme().copyWith(
           backgroundColor: kColorScheme.onPrimaryContainer,
           foregroundColor: kColorScheme.primaryContainer,
+        ),
+        cardTheme: const CardThemeData().copyWith(
+          color: kColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kColorScheme.primaryContainer,
+          ),
+        ),
+        textTheme: ThemeData().textTheme.copyWith(
+          titleLarge: TextStyle(
+            fontWeight: FontWeight.normal,
+            color: kColorScheme.onSecondaryContainer,
+            fontSize: 14,
+          ),
         ),
       ),
       home: Expenses(),

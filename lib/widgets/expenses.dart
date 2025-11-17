b58f99f74b5129fc56a9d9946ce50fcd51dcaf4b
Niamh <85398097+niamh-d@users.noreply.github.com>
2025-11-17 18:11:16 +0200

    expenses chart with chart bars

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -1,3 +1,4 @@
+import 'package:expense_tracker/widgets/chart/chart.dart';
 import 'package:flutter/material.dart';
 
 import 'package:expense_tracker/models/expense.dart';
@@ -95,7 +96,7 @@ class _ExpensesState extends State<Expenses> {
       ),
       body: Column(
         children: [
-          const Text('Chart of expenses'),
+          Chart(expenses: _expenses),
           Expanded(child: mainContent),
         ],
       ),

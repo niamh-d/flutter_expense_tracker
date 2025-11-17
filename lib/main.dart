70d658ca3cdf3f76262b7238cfc7978f0b69a2f0
Niamh <85398097+niamh-d@users.noreply.github.com>
2025-11-17 11:24:05 +0200

    color theme

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,20 @@ import 'package:flutter/material.dart';
 
 import 'package:expense_tracker/widgets/expenses.dart';
 
+var kColorScheme = ColorScheme.fromSeed(
+  seedColor: const Color.fromARGB(255, 96, 59, 181),
+);
 void main() {
-  runApp(const MaterialApp(home: Expenses()));
+  runApp(
+    MaterialApp(
+      theme: ThemeData().copyWith(
+        colorScheme: kColorScheme,
+        appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor: kColorScheme.onPrimaryContainer,
+          foregroundColor: kColorScheme.primaryContainer,
+        ),
+      ),
+      home: Expenses(),
+    ),
+  );
 }
